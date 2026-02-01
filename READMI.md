055e35fb89610058e9262475950794bceaf16d10
vinimax <vini.ciusang@gmail.com>
2026-01-31 22:22:04 -0300

    Initial commit

---
+++ READMI.md
@@ -0,0 +1,1 @@
+###AlgaTransito - Api
---
+++ src/main/java/com/algaworks/algatransito/algatransito/AlgatransitoApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algatransito.algatransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgatransitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgatransitoApplication.class, args);
+    }
+
+}
